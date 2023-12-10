ab5451585e4c91e784558ca373e49bcd0d7e0bf9
Eleazarsimba <eleazarsimba5@gmail.com>
2023-12-10 15:15:53 +0300

    adding mail option

--- src/controllers/Usermanagement.js
+++ src/controllers/Usermanagement.js
@@ -1,6 +1,48 @@
 const bcrypt = require('bcryptjs');
 const connection = require('../helpers/dbConfig');
 const salt = bcrypt.genSaltSync(10);
+const jwt = require('jsonwebtoken');
+const moment = require("moment");
+const cron = require('node-cron');
+const {
+    transformPhoneNumber,
+    generateOTP,
+    validateOTP,
+    sendEmail
+} = require('../helpers/General');
+
+const {
+    SECRETKEY
+} = process.env;
+
+exports.updateLogintrials = () => {
+    const task = cron.schedule('* * * * * *', () => {
+        const currentTime = moment().format('YYYY-MM-DD HH:mm:ss');
+        const updateQuery = `
+            UPDATE tb_users 
+            SET login_trials = 0,
+            update_at = ?
+            WHERE login_trials >= 5 
+            AND DATE_ADD(last_login, INTERVAL 5 MINUTE) <= ?;
+            `;
+        
+        connection.query(updateQuery, [currentTime, currentTime], (error, results) => {
+            if (error) {
+                console.error('Error updating users:', error);
+                return;
+            }
+        
+            if (results.affectedRows > 0) {
+                console.log(`Updated ${results.affectedRows} users.`);
+            } else {
+                console.log('No users found for update.');
+            }
+        });
+    });
+
+    task.start();    
+    return task;
+}
 
 exports.signup = (req, res) => {
     const {
@@ -48,5 +90,120 @@ exports.signup = (req, res) => {
             return res.status(400).send(resObject);
         }
        }
-      });
-    };
+    });
+};
+
+exports.signin = (req, res) => {
+    const {
+        email,
+        password
+    } = req.body;
+    const currentTime = moment().format('YYYY-MM-DD HH:mm:ss');
+
+    var sql = "SELECT * FROM tb_users WHERE email= ? AND login_trials <= 5"
+    var values = [email];
+    connection.query(sql, values, function(error,rows, fields){
+        if(error) {
+            return res.send("Error in signing the user");
+        }else { 
+            if(rows.length > 0) { 
+                bcrypt.compare(password, rows[0].password, function(error, result) {
+                if(result) {
+                    const authOTP = generateOTP();
+                    const subject = "Auth OTP";
+                    const mailbody = `<b>${authOTP}</b> is your verification code`
+                    sendEmail(email, subject, mailbody);
+
+                    const updateQuery = `UPDATE tb_users SET auth_otp =?, otp_createdat =? WHERE email=?`;
+                    const updateValues = [authOTP, currentTime, email];
+                    connection.query(updateQuery , updateValues, function(err, results){
+                        const resObject = {
+                            errorDesc: "An otp has been send to your email, confirm by inputing the otp",
+                            success:"1"
+                        };
+                        return res.status(200).send(resObject)
+                    });
+                }
+                else {
+                    const resObject = {
+                        errorDesc: "Invalid details",
+                        success:"0"
+                    };
+                    return res.status(400).send(resObject);
+                }
+              });
+            } else {
+                const resObject = {
+                    errorDesc: "Too many attempts. Please try again after 5 minutes",
+                    success:"0"
+                };
+                return res.status(400).send(resObject);
+            } 
+        }
+    });
+};
+
+exports.otpAuth = (req, res) => {
+    const {
+        email,
+        auth_otp
+    } = req.body;
+    const currentTime = moment().format('YYYY-MM-DD HH:mm:ss');
+
+    var sql = "SELECT * FROM tb_users WHERE email= ? AND auth_otp= ? AND login_trials <= 5"
+    var values = [email, auth_otp];
+    connection.query(sql, values, function(error,rows, fields){
+        if(error) {
+            return res.send("Error in signing the user");
+        }else { 
+            if(rows.length > 0) {
+                const validOTP = validateOTP(email, rows[0].otp_createdat, 5);
+
+                if(validOTP){
+                    const updateQuery = `UPDATE tb_users SET last_login =?, login_trials =0 WHERE email=?`;
+                    const updateValues = [currentTime, email];
+                    
+                    connection.query(updateQuery , updateValues, function(err, results){
+                        const token = jwt.sign({email: email}, SECRETKEY, {expiresIn: 60 * 60 * 1});  //expires in 1 hour
+                        const resObject = {
+                            id: rows[0].id,
+                            email: rows[0].email,
+                            firstname: rows[0].firstname,
+                            lastname: rows[0].lastname,
+                            phone_number: rows[0].phone_number
+                        };
+                        return res.send({
+                            user: resObject,
+                            token: token, 
+                            success:"1"
+                        });
+                    });
+                }else{
+                    const updateQuery = `UPDATE tb_users SET login_trials =?, last_login =? WHERE email=?`;
+                    const updatedLoginTrials = (rows[0].login_trials) + 1;
+                    const updateValues = [updatedLoginTrials, currentTime, email];
+
+                    connection.query(updateQuery , updateValues, function(err, results){
+                        const resObject = {
+                            errorDesc: "The otp entered is invalid or has expired",
+                            success:"0"
+                        };
+                        return res.send(resObject);
+                    });
+                }
+                
+            } else {
+                const updateQuery = `UPDATE tb_users SET login_trials =login_trials + 1, last_login =? WHERE email=?`;
+                const updateValues = [currentTime, email];
+
+                connection.query(updateQuery , updateValues, function(err, results){
+                    const resObject = {
+                        errorDesc: "Otp confirmation failed or you have exceeded the number of trials",
+                        success:"0"
+                    };
+                    return res.send(resObject);
+                });
+            } 
+        }
+    });
+};
--- src/helpers/General.js
+++ src/helpers/General.js
@@ -1,3 +1,11 @@
+const moment = require("moment");
+const nodeMailer = require('nodemailer');
+
+const {
+    mailUser,
+    mailPass
+} = process.env
+
 //Correct phone number
 const transformPhoneNumber = (input) => {
     // Remove all spaces from the input string
@@ -9,6 +17,58 @@ const transformPhoneNumber = (input) => {
     return transformedString;
 }
 
+//Generate 6 digit otp
+const generateOTP = () => {
+    const otp = Math.floor(100000 + Math.random() * 900000);
+    return otp.toString();
+}
+
+const validateOTP = (userEmail, otpCreatedAt, expirationTimeInMinutes) => {
+    const currentTime = moment().format('YYYY-MM-DD HH:mm:ss');
+    const otpExpirationTime = moment(otpCreatedAt).add(expirationTimeInMinutes, 'minutes').format('YYYY-MM-DD HH:mm:ss');
+
+    if (currentTime <= otpExpirationTime) {
+        console.log('OTP is valid for user:', userEmail);
+        return true;
+    } else {
+        console.log('OTP has expired for user:', userEmail);
+        return false;
+    }
+}
+
+// send email
+const transporter = nodeMailer.createTransport({
+    host: 'smtp.gmail.com',
+    secure: true,
+    auth: {
+      user: mailUser,
+      pass: mailPass
+    }
+});
+
+const sendEmail = (email, subject, mailbody) => {
+    const mailOptions = {
+        to: email,
+        subject: subject,
+        html: mailbody
+    };
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+          res.json({
+            msg: 'fail'
+          })
+        } else {
+          res.json({
+            msg: 'success'
+          })
+        }
+    });
+}
+
 module.exports = {
-    transformPhoneNumber
+    transformPhoneNumber,
+    generateOTP,
+    validateOTP,
+    sendEmail
 }
