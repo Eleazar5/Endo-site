bff99facfc4eb81d9b8ffc1c939ee4886f96d1c3
Eleazarsimba <eleazarsimba5@gmail.com>
2024-01-07 23:21:58 +0300

    Adding backend pagination

--- src/controllers/Usermanagement.js
+++ src/controllers/Usermanagement.js
@@ -240,3 +240,21 @@ exports.getusers = (req, res) => {
         }
     });
 };
+
+exports.getuserspagination = (req, res) => {
+    var sql = "SELECT * FROM tb_users ORDER BY id DESC LIMIT ?, ?"
+    const { page = 1, limit = 2 } = req.query;
+    const offset = (page - 1) * limit;
+
+    connection.query(sql, [offset, Number(limit)], function(error,rows, fields){
+        if(error) {
+            return res.send("Error in getting the users");
+        }else { 
+            const resObject = {
+                data: rows,
+                success:"1"
+            };
+            return res.status(200).send(resObject);
+        }
+    });
+};
