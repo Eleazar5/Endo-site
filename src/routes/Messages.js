6c1628c3d1fcfe595d5417209d6eeca37ecfe40c
Eleazarsimba <eleazarsimba5@gmail.com>
2024-01-10 00:01:24 +0300

    Send sms

--- src/routes/Messages.js
+++ src/routes/Messages.js
@@ -2,10 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const {
-    getAfricaAccountBal
+    getAfricaAccountBal,
+    sendAfricaMessage
 } = require('../controllers/Messages');
 
-router.get('/africa_bal', getAfricaAccountBal);
+const {
+    authMiddleware
+} = require('../validators/auth')
+
+router.get('/africa_bal', authMiddleware, getAfricaAccountBal);
+router.post('/send_sms', authMiddleware, sendAfricaMessage);
 
 module.exports =router;
 
