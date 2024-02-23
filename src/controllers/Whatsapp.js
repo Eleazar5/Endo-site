763a72888039436b1af08a35e9740a348909e944
Eleazarsimba <eleazarsimba5@gmail.com>
2024-02-24 00:24:22 +0300

    changing url

--- src/controllers/Whatsapp.js
+++ src/controllers/Whatsapp.js
@@ -52,19 +52,15 @@ exports.whatsapppost_webhook = (req, res) => {
 };
 
 exports.whatsappget_webhook = (req, res) => {
-  // Parse params from the webhook verification request
   let mode = req.query["hub.mode"];
   let token = req.query["hub.verify_token"];
   let challenge = req.query["hub.challenge"];
 
   // Check if a token and mode were sent
   if (mode && token) {
-    // Check the mode and token sent are correct
     if (mode === "subscribe" && token === VERIFY_TOKEN) {
-      // Respond with 200 OK and challenge token from the request
       res.status(200).send(challenge);
     } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
       res.sendStatus(403);
     }
   }
