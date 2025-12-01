5bf6eb708f5e2d6cc828c681d3f84f51760347df
Eleazar <151408413+Eleazar5@users.noreply.github.com>
2025-12-01 17:56:54 +0300

    Update service.js

--- service.js
+++ service.js
@@ -4,7 +4,7 @@ var Service = require('node-windows').Service;
 var svc = new Service({
   name:'Endo-site',
   description: 'The Endo site backend server.',
-  script: 'C:\\CFT\\Eleazar\\Mobi Duka\\Backend\\Mobi-duka\\index.js'
+  script: 'C:\\Mobi Duka\\Backend\\Mobi-duka\\index.js'
 });
 
 svc.on('install',function(){
